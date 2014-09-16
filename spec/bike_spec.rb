3956332748f0248bf2bd5e245199bd08431ae56d
alexfakhri <alexfakhri@me.com>
2014-09-16 09:11:00 +0100

    Updated methods

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,8 +1,16 @@
-require 'bike.rb'
+require 'bike'
 
 describe Bike do
 	it "Should not be broken after we create it" do
 		the_bike = Bike.new
 		expect(the_bike).not_to be_broken
+	end
+end
+
+describe Bike do
+it "should be able to break" do
+	bike = Bike.new
+	bike.break!
+	expect(bike).to be_broken
 	end
 end
