077976ecaaa03b5722d13c3a6fd55350be34d997
alexfakhri <alexfakhri@me.com>
2014-09-24 19:19:16 +0100

    Restarted project with extensive notes

---
+++ spec/bike_container_spec.rb
@@ -0,0 +1,51 @@
+require 'bike_container'
+require 'bike'
+
+class ContainerHolder; include BikeContainer; end
+
+
+describe BikeContainer do
+	
+	let(:bike) { Bike.new }
+	let(:holder) { ContainerHolder.new }
+
+
+	def fill_holder(holder)
+		holder.capacity.times { holder.dock(bike) }
+	end
+
+	it "should accept a bike" do
+		# we expect the station to have 0 bikes
+		expect(holder.bike_count).to eq(0)
+		# let's dock a bike into the station
+		holder.dock(bike)
+		# now we expect the station to have 1 bike
+		expect(holder.bike_count).to eq(1)
+	end
+
+	it "should release a bike" do
+		holder.dock(bike)
+		holder.release(bike)
+		expect(holder.bike_count).to eq(0)
+	end
+
+	it "should know when if it's full" do
+		expect(holder).not_to be_full
+		fill_holder(holder)
+		expect(holder).to be_full
+	end
+
+	it "should not accpet a bike if it's full" do
+		fill_holder(holder)
+		expect(lambda { holder.dock(bike) }).to raise_error(RuntimeError)
+	end
+
+	it "should provide the list of available bikes" do
+		working_bike, broken_bike = Bike.new, Bike.new
+		broken_bike.break!
+		holder.dock(working_bike)
+		holder.dock(broken_bike)
+		expect(holder.available_bikes).to eq([working_bike])
+	end
+
+end
